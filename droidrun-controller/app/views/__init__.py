e4733a324d304bb52202666bf91c49914daf6bc8
hainc <haidev306@gmail.com>
2025-12-27 19:33:58 +0700

    auto-claude: subtask-4-1 - Update views/__init__.py to export new authentication views

--- droidrun-controller/app/views/__init__.py
+++ droidrun-controller/app/views/__init__.py
@@ -5,5 +5,17 @@ from .workflows import WorkflowsView
 from .executions import ExecutionsView
 from .settings import SettingsView
 from .agent_runner import AgentRunnerView
+from .login import LoginView
+from .registration import RegistrationView
+from .forgot_password import ForgotPasswordView
 
-__all__ = ["DevicesView", "WorkflowsView", "ExecutionsView", "SettingsView", "AgentRunnerView"]
+__all__ = [
+    "DevicesView",
+    "WorkflowsView",
+    "ExecutionsView",
+    "SettingsView",
+    "AgentRunnerView",
+    "LoginView",
+    "RegistrationView",
+    "ForgotPasswordView",
+]
