f4639018732013c280cab7525737f3190820e58e
hainc <haidev306@gmail.com>
2026-01-22 09:50:42 +0700

    Fix: Use ShouldBroadcastNow for immediate device status/accessibility broadcasts

--- laravel-backend/app/Events/DeviceStatusChanged.php
+++ laravel-backend/app/Events/DeviceStatusChanged.php
@@ -6,11 +6,11 @@ use App\Models\Device;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class DeviceStatusChanged implements ShouldBroadcast
+class DeviceStatusChanged implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
